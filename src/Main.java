51e9582e25756951a963616b61567dec081e9003
ThomasKiszely <thomas@bohn-jacobsen.dk>
2024-10-04 10:07:12 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,73 @@
+import java.util.Scanner ;
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        String[] user;
+        user = new String[] {"admin", "Emil", "Annika", "Thomas"};
+        String[] pass;
+        pass = new String[]{"admin", "kevinmagnumsen", "ghost", "psych"};
+
+        for (int i = 0; i < 4; i++) {
+            if (i > 0 && i < 3) {
+                System.out.println("Try again");
+            }
+            else if (i == 3) {
+                System.out.println("Goodbye");
+                break ;
+            }
+
+            System.out.println("Please enter username");
+            String userName = input.nextLine();
+            System.out.println("Please enter password");
+            String passWord = input.nextLine();
+
+            int u = checkUserName(user, userName);
+            int p = checkPassWord(pass, passWord);
+
+
+            if (u == p && u >= 0 && p >= 0) {
+                long[] ans = welcome();
+                if (ans[0] < 10) {
+                    System.out.println("Welcome to SecureServer - the most secure in serving. Time is now " + ans[1] + ":0" + ans[0]);
+                } else {
+                    System.out.println("Welcome to SecureServer - the most secure in serving. Time is now " + ans[1] + ":" + ans[0]);
+                } break;
+            } else {
+                System.out.println("Wrong username or password");
+
+            }
+        }
+    } //end of  main...
+
+    public static int checkUserName(String[] list, String key) {
+        for (int i = 0 ; i < list.length ; i++) {
+            if (key.equals(list[i])) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static int checkPassWord(String[] pass, String passWord) {
+        for (int i = 0 ; i < pass.length ; i++) {
+            if (passWord.equals(pass[i])) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    public static long[] welcome() {
+        long[] ans = new long[2] ;
+        long totalMilliseconds = System.currentTimeMillis();
+        long totalSeconds = totalMilliseconds / 1000;
+        long totalMinutes = totalSeconds / 60;
+        long currentMinutes = totalMinutes % 60;
+        long totalHours = totalMinutes / 60;
+        ans[1] = (totalHours % 24) + 2;
+
+        ans[0] = currentMinutes;
+
+        return ans;
+    }
+}
